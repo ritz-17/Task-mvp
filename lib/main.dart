0253ee1ef1403ee4fc82e9bcd0a17e14a02e4aa8
Mayur Srivastav <mayursrivastava877@gmail.com>
2024-12-29 01:50:14 +0530

    Added routes

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:task_mvp/provider/auth_provider.dart';
-import 'package:task_mvp/provider/timer_provider.dart'; // Import TimerProvider
+import 'package:task_mvp/provider/timer_provider.dart';
+import 'package:task_mvp/screens/dashboard_screen.dart';
+import 'package:task_mvp/screens/employee_task_screen.dart';
 import 'package:task_mvp/screens/login_screen.dart';
 import 'package:task_mvp/screens/profile_screen.dart';
-import 'package:task_mvp/screens/verified_screen.dart';
+import 'package:task_mvp/screens/signup_screen.dart';
 import 'package:task_mvp/screens/splashScreen.dart';
+import 'package:task_mvp/screens/verification_screen.dart';
+import 'package:task_mvp/screens/verified_screen.dart';
 
-import 'screens/dashboard_screen.dart';
-import 'screens/employee_task_screen.dart';
-import 'screens/signup_screen.dart';
-import 'screens/verification_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -36,7 +36,17 @@ class MyApp extends StatelessWidget {
           primaryColor: const Color.fromARGB(255, 122, 90, 248),
           useMaterial3: true,
         ),
-        home: const SplashScreen(),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const SplashScreen(),
+          '/login': (context) => const LoginPage(),
+          '/signup': (context) => const SignupPage(),
+          '/profile': (context) => const ProfileScreen(),
+          '/dashboard': (context) => const DashboardScreen(),
+          '/employeeTasks': (context) => const EmployeeTaskPage(),
+          '/verification': (context) => const VerificationPage(),
+          '/verified': (context) => const VerifiedPage(),
+        },
       ),
     );
   }
